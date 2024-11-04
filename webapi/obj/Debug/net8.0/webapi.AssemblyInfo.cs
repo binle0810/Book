b0841fd602d151081aa3bfa95c6f8b9202fe3717
binle <binlephuoc0810@gmail.com>
2024-11-04 16:40:37 +0700

    fe

--- webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
+++ webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a54ff69fdd58dcbb9edbf8add5429f93bbbe364")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e00d536e66242cb5d65e24fd20e97a1fe003d71")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
